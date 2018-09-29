a0789007bdd427acd19609770ee5dffa3f9602af
Jeff Chasin <jeff@jeffchasin.com>
2018-09-29 16:08:47 -0400

    stub out api calls   - airport name and iata code calls in server/index

--- server/index.js
+++ server/index.js
@@ -6,22 +6,51 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// UNCOMMENT FOR REACT
 // app.use(express.static(__dirname + '/../react-client/dist'));
+app.get('/', function(req, res) {
+  res.send('GET request to home page');
+});
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
+// url:  https://api.aviationdata.systems
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
+// airport(s) by name: /v1/airport/name/{airport_name}
+app.post('/airport/name/:airport_name', function(req, res) {
+  // req.params: { "airport_name": "Hartsfield" }
+  let airportName = 'https://api.aviationdata.systems/v1/airport/name/' + req.params.airport_name;
+  // make the api call by name
+  request.get(airportName, {
+    'auth': {
+      'user': '91785a66-3eef-4600-ad69-07d32352bd27',
+      'pass': 'd758e5fd-6f21-499e-b121-79736978bc83',
+      'sendImmediately': false
     }
   });
 });
+
+// aiport by IATA code
+// /v1/airport/iata/{airport_iata}
+app.post('/airport/iata/:airport_iata', function(req, res) {
+  // req.params: { "airport_iata": "ATL" }
+  let airportIata = 'https://api.aviationdata.systems/v1/airport/iata/' + req.params.airport_iata;
+  // make api call by iata code
+  request.get(airportIata, {
+    'auth': {
+      'user': '91785a66-3eef-4600-ad69-07d32352bd27',
+      'pass': 'd758e5fd-6f21-499e-b121-79736978bc83',
+      'sendImmediately': false
+    }
+  });
+});
+
+// app.get('/items', function (req, res) {
+//   items.selectAll(function(err, data) {
+//     if(err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+var SRC_DIR = path.join(__dirname, 'client/src');
+var DIST_DIR = path.join(__dirname, 'client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -8,15 +8,15 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
-       }
+        }
       }
     ]
   }
